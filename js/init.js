9b760ae4c5f40ed7cc17cda083fd91521dc9c23f
formaeea2 <formaeea2@gmail.com>
2023-07-16 19:34:03 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyDDW3ImvpG4jryUbyEX7qQv37U1HUIPzkc",
+  authDomain: "autic33eea.firebaseapp.com",
+  projectId: "autic33eea",
+  storageBucket: "autic33eea.appspot.com",
+  messagingSenderId: "956740448189",
+  appId: "1:956740448189:web:fb78f15cb16408f8ea2cd4",
+  measurementId: "G-7ZZX1SYBZT"
+    });
